e1bfdad4e5df10fdc9120d4e4e69688caee874a5
rdhox <admin@rdhox.io>
2026-04-01 18:17:29 +0200

    add umami

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -28,6 +28,7 @@ server
         <meta name="description" content="Welcome to my personnal website! You'll find here some informations about my work. Don't hesitate to contact me or follow me on twitter.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="rdhox - Freelancer">
+        <script defer src="https://umami.rdhox.io/script.js" data-website-id="b33486ce-00bf-496c-b6e1-6b4b24f68466"></script>
         ${
           assets.client.css
             ? `<link rel="stylesheet" href="${assets.client.css}">`
@@ -40,16 +41,6 @@ server
         }
     </head>
     <body>
-      <script type="text/javascript">
-        (function(window, document, dataLayerName, id) {
-        window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script');
-        function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString()}document.cookie=a+"="+b+d+"; path=/"}
-        var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
-        var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
-        tags.async=!0,tags.src="https://boxingp4p.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
-        !function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
-        })(window, document, 'dataLayer', 'a8fbb360-d032-41eb-8e7e-b5dee423753a');
-      </script>
       <div id="root">${markup}</div>
     </body>
 </html>`
